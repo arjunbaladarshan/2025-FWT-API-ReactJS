0567e2172ae9905ef2acfb3fce95dad8f7b4c85b
arjunbaladarshan <60723978+arjunbaladarshan@users.noreply.github.com>
2025-09-02 09:30:01 +0530

    without useEffect

--- src/Faculties.js
+++ src/Faculties.js
@@ -18,11 +18,56 @@ function Faculties() {
   console.log("Something");
   
   const foramtedData = data.map((fac)=>{
-    return(<li>{ fac.subjectName } ({fac.subjectFaculty})</li>);
+    return(
+      <>
+        <div class="col-lg-4 col-md-6 align-self-center mb-30 properties-items col-md-6 adv">
+          <div class="item">
+            <a href="property-details.html"><img src="assets/images/property-01.jpg" alt="" /></a>
+            <span class="category">Luxury Villa</span>
+            <h4><a href="property-details.html">{fac.subjectName}</a></h4>
+            <ul>
+              <li>Subject Code: <span>{fac.subjectCode}</span></li>
+              <li>Subject Faculty: <span>{fac.subjectFaculty}</span></li>
+            </ul>
+          </div>
+        </div>
+      </>
+    );
   });
 
   return (
-    <ul>{ foramtedData }</ul>
+    <>
+      <div class="section properties">
+        <div class="container">
+          <ul class="properties-filter">
+            <li>
+              <a class="is_active" href="#!" data-filter="*">Show All</a>
+            </li>
+            <li>
+              <a href="#!" data-filter=".adv">Apartment</a>
+            </li>
+            <li>
+              <a href="#!" data-filter=".str">Villa House</a>
+            </li>
+            <li>
+              <a href="#!" data-filter=".rac">Penthouse</a>
+            </li>
+          </ul>
+          <div class="row properties-box">
+            { foramtedData }
+          </div>
+          <div class="row">
+            <div class="col-lg-12">
+              <ul class="pagination">
+                <li><a href="#">1</a></li>
+                <li><a class="is_active" href="#">2</a></li>
+                <li><a href="#">3</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import ReactDOM from 'react-dom/client';
 import Faculties from './Faculties';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+import About from './About';
+import Home from './Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <>
-    <Faculties/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/faculties' element={<Faculties/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </>
 );
